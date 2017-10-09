218e58b06dfd76e0433365996dc5de73beea1f99
yyj-ode <yyj20141112@163.com>
2017-10-09 19:51:46 +0800

    增加了分页

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/29 0029
+ * Time: 16:51
+ */
+phpinfo();
--- myFunctions.php
+++ myFunctions.php
@@ -120,4 +120,23 @@ function getImg($url = "", $filename = "") {
 //getImg($url, $filename);
 //==================================以上为getImg例子==================================
 
+/**
+ * @param $page -> 当前页数
+ * @param $count -> 数据总条数
+ * @return mixed
+ */
+function getPageList($page,$count){
+    $data['count'] = ceil($count/6);    //总页数
+    $data['total'] = $count;    //总数
+    $data['page'] = $page;  //当前页
+    $data['pre'] = $page-1;   //上一页
+    if($data['pre'] < 1){
+        $data['pre'] = 1;
+    }
+    $data['next'] = $page+1;  //下一页
+    if($data['next'] > $data['count']){
+        $data['next'] = $data['count'];
+    }
+    return $data;
+}
 
